9e5fdea06c56499cc14a22fac1edd04ca764d41b
Aishwary Vishwakarma <aishwaryvishwakarma23@gmail.com>
2024-02-06 17:48:15 +0530

    Added Q. 217

--- Array/TwoSum.js
+++ Array/TwoSum.js
@@ -39,6 +39,19 @@
  * @return {number[]}
  */
 
+// Brute Force
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i,j]
+//       }
+//     }
+//   }
+//   return [];
+// };
+
+// Optimal
 var twoSum = function (nums, target) {
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
